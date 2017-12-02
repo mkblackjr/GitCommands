c8bf3f23a1d52da6bba99ac7524a85053d090875
Mitchell Black <mkblackjr@yahoo.com>
2017-12-02 16:23:55 -0500

    Daily Commit

--- UpdateLocalGit.sh
+++ UpdateLocalGit.sh
@@ -1,24 +1,19 @@
 cd ~/Documents/GithubRepositories/MVS/
 git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
-git fetch --all
 git pull --all
 
 cd ~/Documents/GithubRepositories/WaterQuality/
 git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
-git fetch --all
 git pull --all
 
 cd ~/Documents/GithubRepositories/PythonHelperScripts/
 git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
-git fetch --all
 git pull --all
 
 cd ~/Documents/GithubRepositories/GitCommands/
 git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
-git fetch --all
 git pull --all
 
 cd ~/Documents/GithubRepositories/HomeProjects/
 git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
-git fetch --all
 git pull --all
