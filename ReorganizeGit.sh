e7bfcfb2f1414af503184a5fba435c44a2b35ad0
Mitchell Black <mkblackjr@yahoo.com>
2018-01-03 11:05:29 -0500

    Daily Commit

---
+++ ReorganizeGit.sh
@@ -0,0 +1,49 @@
+cd ~/Documents/GitCommands/
+git checkout development
+git add .
+git commit -m "Daily Commit"
+git push -u origin development
+git merge master
+git checkout master
+git merge development
+git push -u origin master
+
+cd ~/Documents/Professional/MichiganAerospace/GithubRepositories/MVS/
+git checkout development
+git add .
+git commit -m "Daily Commit"
+git push -u origin development
+git merge master
+git checkout master
+git merge development
+git push -u origin master
+
+cd ~/Documents/Professional/MichiganAerospace/GithubRepositories/WaterQuality/
+git checkout development
+git add .
+git commit -m "Daily Commit"
+git push -u origin development
+git merge master
+git checkout master
+git merge development
+git push -u origin master
+
+cd ~/Documents/Professional/MichiganAerospace/GithubRepositories/PythonHelperScripts/
+git checkout development
+git add .
+git commit -m "Daily Commit"
+git push -u origin development
+git merge master
+git checkout master
+git merge development
+git push -u origin master
+
+cd ~/Documents/Personal/HomeProjects/
+git checkout development
+git add .
+git commit -m "Daily Commit"
+git push -u origin development
+git merge master
+git checkout master
+git merge development
+git push -u origin master
